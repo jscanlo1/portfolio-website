f93f8efdacc47dbaf60976d16894b8b9c5c84f02
Johnny Scanlon <johnny.scanlon@caelus-technologies.com>
2025-10-02 15:36:24 +0100

    Remove all old files

--- components/sub-heading-sm.js
+++
@@ -1,9 +0,0 @@
-export default function SubHeadingSM({children}){
-    return(
-        <div className="text-center text-sm text-zinc-700 p-4">
-            <h2 className="">
-                {children}
-            </h2>
-        </div>
-    )
-}
--- components/synth/KeyBoard/KeyBoardNote/KeyBoardNote.js
+++
@@ -1,9 +0,0 @@
-
-
-export default function KeyBoardNote(props) {
-	return (
-		<div className="flex flex-1 relative">
-			{props.children}
-		</div>
-	)
-}
