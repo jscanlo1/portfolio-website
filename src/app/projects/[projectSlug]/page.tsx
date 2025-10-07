10979f2ece94b5cef1c6efb73ea81e866ce25546
Johnny Scanlon <johnny.scanlon@caelus-technologies.com>
2025-10-07 11:06:56 +0100

    Remove remark-gfm

--- src/app/projects/[projectSlug]/page.tsx
+++ src/app/projects/[projectSlug]/page.tsx
@@ -4,7 +4,6 @@ import matter from 'gray-matter';
 import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
 
 type ProjectMetadata = {
     title: string;
